3ba3d951c17a8d916e5413ddfe4cdea1b2668643
Abed Al-Rahman Khateb <abkhateb@microsoft.com>
2018-09-15 13:36:14 +0300

    fixed typos in the train set and added a confusion matrix plot after running the test set

--- CS291K/test.py
+++ CS291K/test.py
@@ -8,6 +8,10 @@ from tensorflow.contrib import learn
 import argparse
 from vocabulary_extractor import generateData
 import os
+import itertools
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+
 
 def tokenizer(iterator):
   """Tokenizer generator.
@@ -21,6 +25,39 @@ def tokenizer(iterator):
   for value in iterator:
     yield value
 
+def plot_confusion_matrix(cm, classes,
+                        normalize=False,
+                        title='Confusion matrix',
+                        cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
 
 def main(emb_path, model_directory, model_metadata):
     """
@@ -68,10 +105,19 @@ def main(emb_path, model_directory, model_metadata):
     print(results)
     print(('Accuracy: %f' % (float(correctPred)/len(labels))))
 
-    statsFilePath = os.path.join(model_directory, "stats.txt")
-    statsFile = open(statsFilePath, "w")
-    statsFile.write(('Accuracy: %f \n' % (float(correctPred)/len(labels))))
-    statsFile.close()
+    cnf_matrix = confusion_matrix(labels, predictions)
+    np.set_printoptions(precision=2)
+
+    class_names = ['Positive', 'Negative']
+    # Plot non-normalized confusion matrix
+    plt.figure()
+    plot_confusion_matrix(cnf_matrix, classes=class_names,
+                          title='Confusion matrix for Sentiment classification')
+    plt.show()
+    #statsFilePath = os.path.join(model_directory, "stats.txt")
+    #statsFile = open(statsFilePath, "w")
+    #statsFile.write(('Accuracy: %f \n' % (float(correctPred)/len(labels))))
+    #statsFile.close()
     #out = sess.run(y,feed_dict)
     #print(out)
     #This will print 60 which is calculated 
